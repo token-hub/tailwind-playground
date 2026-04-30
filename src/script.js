6c9941a9887d72813a8014c8fc5330100010f35b
John Suyang <johnsuyang2118@gmail.com>
2026-04-30 21:05:44 +0800

    Task 38
    Persisting Preference
    
    set theme preference by checking the OS and localstorage

--- src/script.js
+++ src/script.js
@@ -1,15 +1,7 @@
-let lightBtn = document.querySelector("#enable-light-theme");
-let darkBtn = document.querySelector("#enable-dark-theme");
 let html = document.querySelector("html");
 
 if (html) {
-    lightBtn.addEventListener("click", function () {
-        html.classList.remove("dark");
-        html.classList.add("light");
-    });
-
-    darkBtn.addEventListener("click", function () {
-        html.classList.remove("light");
-        html.classList.add("dark");
-    });
+    let userPreferDark = localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches);
+    let theme = userPreferDark ? "dark" : "light";
+    html.classList.add(theme);
 }
