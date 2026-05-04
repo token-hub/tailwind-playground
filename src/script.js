7c1292f27e4a4519ffba35c1839da0e47eada1bd
John Suyang <johnsuyang2118@gmail.com>
2026-05-04 20:06:35 +0800

    Task 50
    Read the current value of --shadow-xl from JavaScript.

--- src/script.js
+++ src/script.js
@@ -1,7 +1,10 @@
-let html = document.querySelector("html");
+// let html = document.querySelector("html");
 
-if (html) {
-    let userPreferDark = localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches);
-    let theme = userPreferDark ? "dark" : "light";
-    html.classList.add(theme);
-}
+// if (html) {
+//     let userPreferDark = localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches);
+//     let theme = userPreferDark ? "dark" : "light";
+//     html.classList.add(theme);
+// }
+
+const styles = getComputedStyle(document.documentElement);
+console.log(styles.getPropertyValue("--shadow-xl"));
